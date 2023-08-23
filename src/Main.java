a1500240901e98274bfa45020dce3a8865702ed0
choijh <hdyoon@fasoo.com>
2023-08-23 15:52:37 +0900

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,1 @@
-public class Main {    public static void main(String[] args) {        System. out.println("Hello World!!" );    }}
+public class Main {    public static void main(String[] args) {        System. out.println("Hedllo World!!" );    }}
