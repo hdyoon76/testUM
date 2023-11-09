5e72633f80f6e4a93b789378e47fc783eed01dc1
choijh <hdyoon@fasoo.com>
2023-11-09 14:55:39 +0900

    Changes

--- src/Main.java
+++ src/Main.java
@@ -1,1 +1,1 @@
-public class Main {    public static void main(String[] args) {        System. out.println("1dHeddd88dddddlddl1o World1!!" );    }}
+public class Main {    public static void main(String[] args) {        System. out.println("1dHefddd88dddddlddl1o World1!!" );    }}
